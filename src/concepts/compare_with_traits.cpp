dad52d000464ecd43e9aafaf8bbd5de8f489072d
Pascal HAVÉ <hpwxf@haveneer.com>
2021-04-21 13:28:27 +0200

    Fix compiler guard for clang

--- src/concepts/compare_with_traits.cpp
+++ src/concepts/compare_with_traits.cpp
@@ -1,9 +1,9 @@
 #include <version>
-#ifndef __cpp_concepts
+#if !(defined(__cpp_concepts) && defined(__cpp_lib_concepts))
 //#region [Feature check]
 #if __has_include("unsupported_features.hpp")
 #include "unsupported_features.hpp"
-REPORT_FEATURES({STR(__cpp_concepts)});
+REPORT_FEATURES({STR(__cpp_concepts), STR(__cpp_lib_concepts)});
 #else
 #error "Unsupported feature"
 #endif
