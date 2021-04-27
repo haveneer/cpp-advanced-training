5489bed754d6f93e5f4d1c4e6ef065fe75740bd3
Pascal HAVÉ <hpwxf@haveneer.com>
2021-04-27 15:32:53 +0200

    Add bug report in GCC
    This test will fail when the bug will be solved.

---
+++ src/ranges/ranges_join_bug_100290.cpp
@@ -0,0 +1,27 @@
+#include <version>
+#ifndef __cpp_lib_ranges
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES({STR(__cpp_lib_ranges)});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
+#include <ranges>
+#include <cassert>
+
+int main() {
+  auto v =
+      std::views::single(1)                                                        //
+      | std::views::transform([](const auto &s) { return std::views::single(s); }) //
+      | std::views::join;
+  
+  // When this assertion will fail, the bug will be solved
+  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100290
+  assert(std::ranges::next(v.begin()) != v.end()); 
+}
+
+#endif
