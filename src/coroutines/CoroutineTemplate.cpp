7ea142bf51ae98378cdd9dac95453f5746dade36
Pascal HAVÉ <hpwxf@haveneer.com>
2021-06-14 16:08:10 +0200

    Add a template for coroutine examples

---
+++ src/coroutines/CoroutineTemplate.cpp
@@ -0,0 +1,29 @@
+#include <version>
+#if __has_include(<experimental/coroutine>)
+#define __cpp_lib_coroutine
+#endif
+#ifndef __cpp_lib_coroutine
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES({STR(__cpp_lib_coroutine)});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
+//#region [Collapse all]
+#if __has_include(<coroutine>)
+#include <coroutine>
+#elif __has_include(<experimental/coroutine>)
+#include <experimental/coroutine>
+namespace std {
+using namespace std::experimental;
+}
+#endif
+//#endregion
+
+int main() {}
+
+#endif
