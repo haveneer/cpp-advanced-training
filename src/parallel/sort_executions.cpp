ccf6cc5605f1df5a797c86bd5f767ac20ebcc9b0
Pascal Havé <hpwxf@haveneer.com>
2021-07-15 20:36:28 +0200

    Add two parallel STL examples/benchmarks

---
+++ src/parallel/sort_executions.cpp
@@ -0,0 +1,71 @@
+#include <version>
+#ifndef __cpp_lib_parallel_algorithm
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES({STR(__cpp_lib_parallel_algorithm)});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
+#include <algorithm>
+#include <chrono>
+#include <execution>
+#include <iostream>
+#include <random>
+#include <vector>
+
+struct no_policy_t {
+} no_policy;
+
+template <typename T>
+void test(const T &policy, const std::vector<double> &data, const int repeat,
+          const char *message) {
+  for (int i = 0; i < repeat; ++i) {
+    std::vector<double> curr_data(data);
+
+    const auto start = std::chrono::steady_clock::now();
+    if constexpr (std::is_same_v<T, no_policy_t>)
+      std::sort(curr_data.begin(), curr_data.end());
+    else
+      std::sort(policy, curr_data.begin(), curr_data.end());
+    const auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << message << ' '
+              << std::chrono::duration<double, std::milli>(diff).count() << " ms\n";
+  }
+  std::cout << '\n';
+}
+
+int main() {
+  // Test samples and repeat factor
+  constexpr size_t samples{10'000'000};
+  constexpr int repeat{3};
+
+  // Fill a vector with samples numbers
+  std::random_device rd;
+  std::mt19937_64 mre(rd());
+  std::uniform_real_distribution<double> urd(0.0, 1.0);
+
+  std::vector<double> data(samples);
+  for (auto &e : data) {
+    e = urd(mre);
+  }
+
+  // Sort data using different execution policies
+  std::cout << "no execution policy\n";
+  test(no_policy, data, repeat, "Elapsed time");
+
+  std::cout << "std::execution::seq\n";
+  test(std::execution::seq, data, repeat, "Elapsed time");
+
+  std::cout << "std::execution::par\n";
+  test(std::execution::par, data, repeat, "Elapsed time");
+
+  std::cout << "std::execution::par_unseq\n";
+  test(std::execution::par_unseq, data, repeat, "Elapsed time");
+}
+
+#endif
---
+++ src/parallel/stl_parallel_insight.cpp
@@ -0,0 +1,164 @@
+#include <version>
+#ifndef __cpp_lib_parallel_algorithm
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES({STR(__cpp_lib_parallel_algorithm)});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
+//#region [Colorization]
+#include <iomanip>
+#include <iostream>
+#if __has_include("colorize.hpp")
+#include "colorize.hpp"
+#else
+template <typename T> struct Colorize { T x; };
+template <typename T>
+std::ostream &operator<<(std::ostream &o, const Colorize<T> &t) {
+  return o << t.x;
+}
+template <typename T> Colorize(T &&t) -> Colorize<T>;
+#endif
+//#endregion
+
+//#region [Many includes]
+#include <algorithm>
+#include <atomic>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <execution>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <thread>
+#include <vector>
+//#endregion
+
+struct SyncCout {
+  SyncCout() : oss{} {
+    std::ostream o(oss.rdbuf()); // safe RAII flags restore
+    o << std::hex << "[" << Colorize{std::this_thread::get_id()} << "] ";
+  }
+  ~SyncCout() {
+    oss << '\n';
+    std::scoped_lock lock(m_mutex);
+    std::cout << oss.str();
+  }
+  template <typename T> auto &operator<<(T &&val) { return oss << val; }
+  std::ostringstream oss;
+  static inline std::mutex m_mutex;
+};
+
+template <typename T> struct Inspector {
+  Inspector(const T *origin)
+      : origin{origin}, start{std::chrono::high_resolution_clock::now()} {}
+  Inspector(const Inspector &that)
+      : origin{that.origin}, start{std::chrono::high_resolution_clock::now()} {}
+  Inspector(Inspector &&that)
+      : origin{that.origin}, start{std::chrono::high_resolution_clock::now()} {}
+  Inspector &operator=(const Inspector &) = delete;
+  Inspector &operator=(Inspector &&) = delete;
+  ~Inspector() {
+    if (first != last) {
+      // auto diff = std::chrono::high_resolution_clock::now() - start;
+      // SyncCout{}
+      //     << std::chrono::duration_cast<std::chrono::microseconds>(diff).count()
+      //     << "_us on range [" << first << ":" << last
+      //     << "] = " << (last - first + 1);
+      {
+        std::lock_guard guard(id_mutex);
+        auto [inserter, is_new_entry] =
+            id_map.insert({std::this_thread::get_id(),
+                           {0, std::numeric_limits<std::ptrdiff_t>::max(), 0, 0}});
+        ChunkData &data = inserter->second;
+        const auto size = last - first + 1;
+        data.call_count++;
+        data.min_size = std::min(data.min_size, size);
+        data.max_size = std::max(data.max_size, size);
+        data.cumul_size += size;
+      }
+    }
+  }
+  void inspect(const T &i) const {
+    std::ptrdiff_t pos = &i - origin;
+    if (first == std::numeric_limits<std::ptrdiff_t>::max())
+      first = pos;
+    last = pos;
+  }
+  const T *origin = nullptr;
+  std::chrono::time_point<std::chrono::high_resolution_clock> start;
+  mutable std::ptrdiff_t first = std::numeric_limits<std::ptrdiff_t>::max();
+  mutable std::ptrdiff_t last = std::numeric_limits<std::ptrdiff_t>::max();
+
+  struct ChunkData {
+    std::size_t call_count;
+    std::ptrdiff_t min_size;
+    std::ptrdiff_t max_size;
+    std::size_t cumul_size;
+  };
+  static inline std::mutex id_mutex;
+  static inline std::map<std::thread::id, ChunkData> id_map;
+};
+
+int main() {
+  std::vector<double> v(10'000'000, 0);
+  std::iota(std::begin(v), std::end(v), 0);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  std::transform(std::execution::par, //
+                 std::begin(v), std::end(v), std::begin(v),
+                 [inspector = std::move(Inspector{std::data(v)})](const auto &v) {
+                   inspector.inspect(v); // HINT comment this to disable analysis
+                   return std::sqrt(std::sin(v) * std::cos(v));
+                 });
+
+  //#region [Do stats]
+  auto diff = std::chrono::high_resolution_clock::now() - start;
+  SyncCout{} << "Total : "
+             << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
+             << "_ms";
+  // safe not concurrent area
+  using InspectorT = Inspector<decltype(v)::value_type>;
+  using ChunkData = InspectorT::ChunkData;
+  const auto &id_map = InspectorT::id_map;
+  if (!id_map.empty()) {
+    auto [min_chunk_element, max_chunk_element] =
+        std::minmax_element(id_map.begin(), id_map.end(), [](auto a, auto b) {
+          const ChunkData &da = a.second;
+          const ChunkData &db = b.second;
+          return da.call_count < db.call_count;
+        });
+    auto min_chunk_size =
+        std::min_element(id_map.begin(), id_map.end(), [](auto a, auto b) {
+          const ChunkData &da = a.second;
+          const ChunkData &db = b.second;
+          return da.min_size < db.min_size;
+        })->second.min_size;
+    auto max_chunk_size =
+        std::min_element(id_map.begin(), id_map.end(), [](auto a, auto b) {
+          const ChunkData &da = a.second;
+          const ChunkData &db = b.second;
+          return da.max_size < db.max_size;
+        })->second.max_size;
+    auto chunk_count =
+        std::accumulate(id_map.begin(), id_map.end(), 0, [](auto acc, auto v) {
+          const ChunkData &dv = v.second;
+          return acc + dv.call_count;
+        });
+    SyncCout{} << "using " << id_map.size() << " threads called ["
+               << min_chunk_element->second.call_count << ":"
+               << max_chunk_element->second.call_count << "] times with sizes in ["
+               << min_chunk_size << ":" << max_chunk_size << "~"
+               << v.size() / chunk_count << "]";
+  }
+  //#endregion
+}
+
+#endif
