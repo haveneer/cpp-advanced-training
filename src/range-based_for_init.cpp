982f356cc3f3cb8336266aea4a660517d6053147
Pascal Havé <hpwxf@haveneer.com>
2021-09-13 13:23:53 +0200

    Add a workaround for MSVC 19.29 with my_cpp_feature_range_based_for_loop_with_structured_binding_init

--- src/range-based_for_init.cpp
+++ src/range-based_for_init.cpp
@@ -1,3 +1,16 @@
+#include <version>
+#if !defined(my_cpp_feature_range_based_for_loop_with_structured_binding_init)
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES(
+    {STR(my_cpp_feature_range_based_for_loop_with_structured_binding_init)});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
 //#region [Collapse all]
 #include <iostream>
 #include <string_view>
@@ -20,8 +33,10 @@ int main() {
   }
   std::cout << "}\n\n";
 
-  // More advanced
+  // More advanced // MSVC 19.29 has a bug here (cf CMakeLists.txt)
   for (const auto &[word, names] = S{}; auto name : names) {
     std::cout << word << ' ' << name << '\n';
   }
 }
+
+#endif
