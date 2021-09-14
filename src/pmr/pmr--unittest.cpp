ffeffb83eca2356de465278b8feffc01b33338f7
Pascal Havé <hpwxf@haveneer.com>
2021-09-14 15:33:43 +0200

    Add std::vector<std::string> as default example

--- src/pmr/pmr--unittest.cpp
+++ src/pmr/pmr--unittest.cpp
@@ -16,10 +16,16 @@ REPORT_FEATURES({STR(__cpp_lib_memory_resource)});
 #include <iostream>
 #include <memory_resource>
 #include <string>
+#include <vector>
+#include <list>
+#include <type_traits>
 
 template <template <typename T> class Collection, typename Type>
 TrackNew::Status test_body(auto default_value) {
-  TrackNew::reset();
+
+  constexpr bool is_polymorphic_collection =
+      std::is_same_v<typename Collection<Type>::allocator_type,
+                     std::pmr::polymorphic_allocator<Type>>;
 
   // buffer is a static array allocated on the stack
   std::array<std::byte, 200'000> buffer;
@@ -28,13 +34,29 @@ TrackNew::Status test_body(auto default_value) {
   // using memory allocated on the stack through buffer
   std::pmr::monotonic_buffer_resource pool{buffer.data(), buffer.size()};
 
-  Collection<Type> collection{&pool};
+  TrackNew::reset();
+
+  auto collection = [&] {
+    if constexpr (is_polymorphic_collection) {
+      return Collection<Type>{&pool};
+    } else {
+      return Collection<Type>();
+    }
+  }();
 
   for (int i = 0; i < 1000; ++i) {
-    collection.emplace_back("A string that beats SSO or Small String Optimization");
+    collection.emplace_back(default_value);
   }
 
   return TrackNew::status();
+}
+
+TEST(PMR, std_string_in_vector_should_use_many_new) {
+  TrackNew::Status status = test_body<std::vector, std::string>(
+      "A string that beats SSO or Small String Optimization");
+  EXPECT_GT(status.numAlloc, 0);
+  EXPECT_GT(status.sumSize, 0);
+  std::cout << status << '\n';
 }
 
 TEST(PMR, std_string_in_pmr_vector_should_use_new) {
