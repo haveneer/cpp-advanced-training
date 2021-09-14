bccb5b01813022ce71253825735bb208c9b923ac
Pascal Havé <hpwxf@haveneer.com>
2021-09-14 12:46:41 +0200

    Add PMR unit test as demo
    Includes a Memory Tracker inspired by Nicolai M. Josuttis

---
+++ include/TrackNew.hpp
@@ -0,0 +1,86 @@
+#ifndef TRACKNEW_HPP
+#define TRACKNEW_HPP
+// Inspired by Nicolai M. Josuttis
+
+#include <cstdio>  // for printf() : do not use cout (which can do allocations)
+#include <cstdlib> // for C++17 std::malloc(), std::aligned_alloc(), std::free
+#include <iostream>
+#include <new> // for std::align_val_t
+
+class TrackNew {
+public:
+  struct Status {
+    int numAlloc = 0;   // num malloc calls
+    size_t sumSize = 0; // bytes allocated so far
+
+    friend std::ostream &operator<<(std::ostream &o, const Status &status) {
+      return o << status.numAlloc << " allocations for " << status.sumSize << " byte"
+               << ((status.sumSize > 1) ? "s" : "");
+    }
+  };
+
+private:
+  static inline bool m_do_trace = false; // tracing enabled
+  static inline bool inNew = false;      // don't track output inside new overloads
+  static inline Status m_status{0, 0};
+
+public:
+  static void reset() { // reset new/memory counters
+    m_status.numAlloc = 0;
+    m_status.sumSize = 0;
+  }
+
+  static void trace(bool b) { // enable/disable tracing
+    m_do_trace = b;
+  }
+
+  // implementation of tracked allocation:
+  static void *allocate(std::size_t size, std::size_t align, const char *call) {
+    // track and trace the allocation:
+    ++m_status.numAlloc;
+    m_status.sumSize += size;
+
+    void *p = (align == 0) ? std::malloc(size) : std::aligned_alloc(align, size);
+
+    if (m_do_trace) {
+      // DON'T use std::cout here because it might allocate memory
+      // while we are allocating memory (core dump at best)
+      printf("#%d %s ", m_status.numAlloc, call);
+      printf("(%zu bytes, ", size);
+      if (align > 0) {
+        printf("%zu-byte aligned) ", align);
+      } else {
+        printf("def-aligned) ");
+      }
+      printf("=> %p (total: %zu bytes)\n", (void *)p, m_status.sumSize);
+    }
+    return p;
+  }
+
+  static Status status() { return m_status; }
+};
+
+[[nodiscard]] void *operator new(std::size_t size) {
+  return TrackNew::allocate(size, 0, "::new");
+}
+
+[[nodiscard]] void *operator new(std::size_t size, std::align_val_t align) {
+  return TrackNew::allocate(size, static_cast<size_t>(align), "::new aligned");
+}
+
+[[nodiscard]] void *operator new[](std::size_t size) {
+  return TrackNew::allocate(size, 0, "::new[]");
+}
+
+[[nodiscard]] void *operator new[](std::size_t size, std::align_val_t align) {
+  return TrackNew::allocate(size, static_cast<size_t>(align), "::new[] aligned");
+}
+
+void operator delete(void *p) noexcept { std::free(p); }
+void operator delete(void *p, std::size_t) noexcept { ::operator delete(p); }
+void operator delete(void *p, std::align_val_t) noexcept { std::free(p); }
+void operator delete(void *p, std::size_t, std::align_val_t align) noexcept {
+  ::operator delete(p, align);
+}
+
+#endif // TRACKNEW_HPP
---
+++ src/pmr/pmr--unittest.cpp
@@ -0,0 +1,41 @@
+﻿#include "TrackNew.hpp"
+#include <array>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory_resource>
+
+template <template <typename T> class Collection, typename Type>
+TrackNew::Status test_body(const Type &default_value) {
+  TrackNew::reset();
+
+  // buffer is a static array allocated on the stack
+  std::array<std::byte, 200'000> buffer;
+
+  // an instance of monotonic_buffer_resource
+  // using memory allocated on the stack through buffer
+  std::pmr::monotonic_buffer_resource pool{buffer.data(), buffer.size()};
+
+  Collection<Type> collection{&pool};
+
+  for (int i = 0; i < 1000; ++i) {
+    collection.emplace_back();
+  }
+
+  return TrackNew::status();
+}
+
+TEST(PMR, std_string_in_pmr_vector_should_use_new) {
+  TrackNew::Status status = test_body<std::pmr::vector, std::string>(
+      "A string that beats SSO or Small String Optimization");
+  EXPECT_GT(status.numAlloc, 0);
+  EXPECT_GT(status.sumSize, 0);
+  std::cout << status << '\n';
+}
+
+TEST(PMR, std_string_in_pmr_vector_should_not_use_new) {
+  TrackNew::Status status = test_body<std::pmr::vector, std::pmr::string>(
+      "A string that beats SSO or Small String Optimization");
+  EXPECT_EQ(status.numAlloc, 0);
+  EXPECT_EQ(status.sumSize, 0);
+  std::cout << status << '\n';
+}
