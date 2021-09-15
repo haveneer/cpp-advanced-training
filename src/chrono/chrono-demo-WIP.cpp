bfc0e7869724cde598925124a3a1d67dcdedc575
Pascal Havé <hpwxf@haveneer.com>
2021-09-15 13:53:41 +0200

    Add C++20 chrono demo
    Mostly to check is the feature is available

---
+++ src/chrono/chrono-demo-WIP.cpp
@@ -0,0 +1,33 @@
+#include <version>
+#if !defined(__cpp_lib_chrono) || __cpp_lib_chrono < 201907L
+//#region [Feature check]
+#if __has_include("unsupported_features.hpp")
+#include "unsupported_features.hpp"
+REPORT_FEATURES({STR(__cpp_lib_chrono) "_201907L"});
+#else
+#error "Unsupported feature"
+#endif
+//#endregion
+#else
+
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+  std::istringstream in{"Thu, 9 Jan 2014 12:35:34.123 +0000"};
+  std::chrono::sys_time<std::chrono::seconds> tp;
+  std::string format = "%a, %d %b %Y %T %z";
+  in >> std::chrono::parse(format, tp);
+  auto tp_days = floor<std::chrono::days>(tp);
+  std::chrono::hh_mm_ss hms{tp - tp_days};
+  std::cout << tp << '\n';
+  std::cout << "Number of days         = " << tp_days.time_since_epoch() << '\n';
+  std::cout << "Number of hours        = " << hms.hours() << '\n';
+  std::cout << "Number of minutes      = " << hms.minutes() << '\n';
+  std::cout << "Number of seconds      = " << hms.seconds() << '\n';
+  std::cout << "Number of milliseconds = " << hms.subseconds() << '\n';
+}
+
+#endif
