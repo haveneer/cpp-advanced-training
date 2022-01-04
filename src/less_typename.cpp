39d9b48ec1925e4251e40b8fa950c9f4af11811e
Pascal Havé <hpwxf@haveneer.com>
2022-01-04 11:19:55 +0100

    Add CMake feature detection for "less typename"

--- src/less_typename.cpp
+++ src/less_typename.cpp
@@ -7,7 +7,7 @@ template <typename T> std::string type() { if (std::is_same_v<std::remove_extent
 #endif              // clang-format on
 
 #include <vector>
-#if __cplusplus > 201703L
+#ifdef my_cpp_feature_less_typename
 // C++20 compilers can infer non-ambiguous typename keyword
 // Cf proposal P0634: https://wg21.link/P0634
 #define OPTIONAL_TYPENAME
@@ -15,7 +15,7 @@ template <typename T> std::string type() { if (std::is_same_v<std::remove_extent
 // They are still required in C++17
 #define OPTIONAL_TYPENAME typename
 #endif
-#define REQUIRED_TYPENAME typename
+
 #include <iostream>
 
 struct TypeT {
@@ -27,12 +27,20 @@ struct TypeT {
   constexpr static void *X1 = nullptr;
   using X2 = int;
 };
+
+struct Dependent_x_is_a_value {
+  constexpr static int x = 1;
+};
+
+struct Dependent_x_is_a_type {
+  using x = int;
+};
 //#endregion
 
 // OK, return type of a function declaration at global scope
 template <class T> OPTIONAL_TYPENAME T::R f1();
 
-template <class T> struct S : OPTIONAL_TYPENAME T::C { // OK, inheritance
+template <class T> struct S {
 
   using V = OPTIONAL_TYPENAME T::V; // OK, in a defining-type-id
 
@@ -43,8 +51,8 @@ template <class T> struct S : OPTIONAL_TYPENAME T::C { // OK, inheritance
 };
 
 template <class T> void f2() {
-  void (*pf1)(T::X1);          // Meaningful typename which Distinguishes
-                               // variable pf of type void* initialized with T::X and
+  void (*pf1)(T::X1);          // Meaningful typename which distinguishes
+                               // variable of type void* initialized with T::X1
   void (*pf2)(typename T::X2); // and pointer to function T::X2 -> void
   //#region [Check types]
   std::cout << "type(pf1) is " << type(pf1) << std::endl;
@@ -53,12 +61,27 @@ template <class T> void f2() {
 }
 
 template <class T>
-void f3(REQUIRED_TYPENAME T::R); // Would be ill-formed, declare a void variable
+void f3(typename T::R); // Would be ill-formed, declare a void variable
 template <class T> void f4() {
-  void g(REQUIRED_TYPENAME T::X); // T::X at block scope does not denote a type
-  //                              // (attempt to declare a void variable)
+  void g(typename T::X); // T::X at block scope does not denote a type
+  //                     // (attempt to declare a void variable)
+}
+
+template <class T> void f5(int i) {
+  T::x *i; // type or value ? (cf [temp.res]p6)
+}
+
+template <typename T> void f6(const std::vector<T> &v) {
+  // std::vector<T>::const_iterator is a dependent name and assumed as a non-type
+  typename std::vector<T>::const_iterator it = v.begin(); // typename required
+  typename std::vector<T>::const_iterator typedef iter_t; // typename required
 }
 
 //#region [main]
-int main() { f2<TypeT>(); }
+int main() {
+  f2<TypeT>();
+  f5<Dependent_x_is_a_value>(1); // OK: T::x is a value
+  // f5<Dependent_x_is_a_type>(1);  // error: dependent-name 'T::x' is parsed as a
+  //                                //  non-type, but instantiation yields a type
+}
 //#endregion
